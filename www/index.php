0cda2b0caa21d41fef399f8ff1542077b026d716
GestaPham <46752866+GestaPham@users.noreply.github.com>
2022-08-15 05:51:18 +0700

    login-system-1.0
    new login system

--- www/index.php
+++ www/index.php
@@ -1,3 +1,28 @@
 <?php
-    echo "<h1>Hello, world!</h1>"
+session_start();
+
+if (isset($_POST["logout"])){
+    unset($_SESSION['User']);
+}  
+if (!isset($_SESSION["User"])){
+    header("Location: /www/login/login.php");
+    exit();
+}
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>welcome</h1>
+    <form method="post">
+        <input type="hidden" name="logout" value="1">
+        <input type="submit" value="logout">
+    </form>
+</body>
+</html>
--- www/login/login.php
+++ www/login/login.php
@@ -1,3 +1,39 @@
+<?php
+/// HANDLE LOGIN ///
+
+session_start();
+$account_list = file_get_contents("../../account.db");
+$json_account_list = json_decode($account_list, true);
+$acc_len = count($json_account_list);
+
+
+if (isset($_POST["Uname"]) && isset($_POST["Pword"]) && !isset($_SESSION["User"])){
+    for ($i=0; $i < $acc_len; $i++){
+        if ($json_account_list[$i]["username"] == $_POST["Uname"]){
+            $pw_hash = $json_account_list[$i]["password"];
+            if (password_verify($_POST["Pword"], $pw_hash)){
+                $_SESSION["User"] = $_POST["Uname"];
+                break;
+            }
+        }
+    }
+    if (!isset($_SESSION["User"])){
+        $login_failed = true;
+    }
+}
+
+if (isset($_SESSION["User"])){
+    header("Location: /www/index.php");
+    exit();
+}
+
+if ($login_failed){
+    /// FAIL MESSAGE ///
+    echo "<br><br><br>INVALID USERNAME/PASSWORD";
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -14,13 +50,13 @@
 <!--HEADER START-->
 <header>
     <!--need navigation item ideas-->
-    <a href="../index.php" class="navigation" id="navigation">Navigation</a>
+    <a href="#" class="navigation" id="navigation">Navigation</a>
 </header>
 <!--HEADER END-->
 
 <section>
     <!--Form START-->
-    <form>
+    <form method="POST">
         <div class="container">
             <div class="LoginBx">
                 <div class="Lleft"></div>
@@ -29,7 +65,7 @@
                     <br>
                     <div class="name">
                         <label for="Uname">Username</label>
-                        <input type="text" class="field" id="Uname" placeholder="Username" required>
+                        <input type="text" class="field" name="Uname" placeholder="Username" required>
                         <span><a href="#">forgot username?</a></span>
                     </div>
 
@@ -38,7 +74,7 @@
 
                     <div class="passW">
                         <label for="Pword">Password</label>
-                        <input type="text" class="field" id="Pword" placeholder="Password" required>
+                        <input type="password" class="field" name="Pword" placeholder="Password" required>
                         <span><a href="#">forgot password?</a></span>
                     </div>
 
@@ -47,7 +83,6 @@
                     <div class="rememberMe">
                         <input type="checkbox" value="lsRememberMe" id="rememberMe">
                         <label for="rememberMe">Remember me</label>
-
                     </div>
 
                     <br>
