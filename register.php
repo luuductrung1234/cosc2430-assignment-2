3389456f31f81686b59d220648b4f802d783c9d9
yunjae Kim <yunjae2094@gmail.com>
2022-08-15 10:50:46 +0700

    lazada register page 1

---
+++ register.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+  <header>
+    <nav>
+      <!-- nav -->
+    </nav>    
+  </header>  
+  <main id="register_main">
+    <h2>
+      Welcome to LAZADA
+    </h2>
+    <section id="register_container">
+
+      <h3>
+        Who are you?
+      </h3>
+
+      <div id="register_row">
+        
+        <div class="register_col">
+          <img src="./img/vendor1.jpg" alt="vendor">
+          <h4>
+            vendor
+          </h4>
+        </div>
+
+        
+     
+
+        <div class="register_col">        
+      
+          <img src="./img/customer1.jpg" alt="customer">
+ 
+          <h4>
+            customer
+          </h4>
+        </div>
+
+        <div class="register_col">     
+
+          <img src="./img/delivery.jpg" alt="delivery">
+
+          <h4>
+            shipper
+          </h4>
+        </div>
+
+      </div>
+    </section>
+
+      
+
+    <div id="register_button">
+        <button class="register_button">
+          go back
+        </button>
+        <button class="register_button">
+          continue
+        </button>
+    </div>
+    
+  </main>
+  <footer>
+    <!-- footer -->
+  </footer>
+</body>
+</html>
