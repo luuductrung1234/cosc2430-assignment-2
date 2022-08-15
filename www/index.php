e8db11f648c7e2602da020f3e621fd97e126ffe0
Thomas Luu (Tom) <thomas.luu@transwap.com>
2022-08-15 15:07:59 +0700

    define all data files

--- www/index.php
+++ www/index.php
@@ -5,7 +5,7 @@ if (isset($_POST["logout"])){
     unset($_SESSION['User']);
 }  
 if (!isset($_SESSION["User"])){
-    header("Location: /www/login/login.php");
+    header("Location: ./login/login.php");
     exit();
 }
 ?>
@@ -17,6 +17,8 @@ if (!isset($_SESSION["User"])){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <h1>welcome</h1>
@@ -24,5 +26,7 @@ if (!isset($_SESSION["User"])){
         <input type="hidden" name="logout" value="1">
         <input type="submit" value="logout">
     </form>
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 </body>
 </html>
