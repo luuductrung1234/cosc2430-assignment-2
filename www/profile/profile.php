156510d4948b3397d1d6f75079219e1a3472c4e7
Leon Do <91256597+Random-Palico@users.noreply.github.com>
2022-08-19 18:35:31 +0700

    Change name

--- www/profile/profile.php
+++ www/profile/profile.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/js/bootstrap.bundle.min.js">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js">
-    <link rel="stylesheet" href="account-style.css">
+    <link rel="stylesheet" href="profile-style.css">
 </head>
 
 <body>
@@ -23,7 +23,7 @@
                     <span class="font-weight-bold">User</span><span class="text-black-50">user@gmail.com</span>
                 </div>
 
-                <script src="account-script.js"></script>
+                <script src="profile-script.js"></script>
             </div>
             <div class="col-md-5 border-right">
                 <div class="p-3 py-5 type2">
