7f1b699c4ef5903bc2d7bad642d09a5504ba38f6
Thomas Luu (Tom) <thomas.luu@transwap.com>
2022-08-23 22:01:29 +0700

    fix shipper page

--- www/controllers/ShipperController.php
+++ www/controllers/ShipperController.php
@@ -7,13 +7,18 @@ namespace Controllers;
 require_once SEED_WORK_PATH . "View.php";
 
 use SeedWork\View;
+use Services\AuthenticationService;
+use Services\DataAccessService;
 
 class ShipperController
 {
     public function index(): string
     {
+        $account = AuthenticationService::getAccount();
+        $profile = DataAccessService::getProfile($account["profileId"], $account["role"]);
         $viewData = [
             "_title" => "Shipper",
+            "_avatar" => $profile["picture"],
             "shipper" => [
                 "name" => "John"
             ]
