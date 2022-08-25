b48158aa1945fca53afaa8d40d86b22f08d8b9eb
Leon Do <91256597+Random-Palico@users.noreply.github.com>
2022-08-25 14:25:39 +0700

    update footer, shipper, order detail

--- www/views/shipper/index-script.js
+++ www/views/shipper/index-script.js
@@ -1,9 +1,1 @@
 document.getElementsByClassName("shipper-header-title")[0].innerHTML = "Shipper Home Page";
-
-$(document).ready(function () {
-
-    $('.toggle-btn').click(function () {
-        $(this).toggleClass('active').siblings().removeClass('active');
-    });
-
-});
--- www/views/shipper/index.php
+++ www/views/shipper/index.php
@@ -30,8 +30,7 @@
                                     <div class="table-responsive table-bordered">
                                         <table class="table">
                                             <thead>
-                                                <tr>
-                                                    <th>Confirm</th>
+                                                <tr class="text-center">
                                                     <th>Order #ID</th>
                                                     <th>Customer name</th>
                                                     <th>Item</th>
@@ -39,67 +38,58 @@
                                                     <th>Order Status</th>
                                                     <th>Total</th>
                                                     <th>Date</th>
+                                                    <th>View order</th>
+                                                    <th>Deliver</th>
                                                 </tr>
                                             </thead>
-                                            <tbody class="table-body">
+                                            <tbody class="table-body text-center">
                                                 <tr>
-                                                    <td class="text-center">
-                                                        <div class="toggle-btn">
-                                                            <div class="inner-circle"></div>
-                                                        </div>
-                                                    </td>
                                                     <td>#9999999</td>
                                                     <td>Name</td>
                                                     <td>Product</td>
                                                     <td>item</td>
-                                                    <td><span class="badge bg-success">Fullfilled</span></td>
+                                                    <td><span class="badge bg-success">Delivered</span></td>
                                                     <td>$124.00</td>
                                                     <td>Today</td>
+                                                    <td><a href="index-detail.php"><button type="button" class="btn btn-dark btn-sm text-center">View order</button></a></td>
+
                                                 </tr>
                                                 <tr>
-                                                    <td class="text-center">
-                                                        <div class="toggle-btn">
-                                                            <div class="inner-circle"></div>
-                                                        </div>
-                                                    </td>
                                                     <td>#9999999</td>
                                                     <td>Name</td>
                                                     <td>Product</td>
                                                     <td>item</td>
-                                                    <td><span class="badge bg-info">Confirmed</span></td>
+                                                    <td><span class="badge bg-info">Active</span></td>
                                                     <td>$34.00</td>
                                                     <td>Yesterday</td>
+                                                    <td><a href="index-detail.php"><button type="button" class="btn btn-dark btn-sm text-center">View order</button></a></td>
+                                                    <td><button type="button" class="btn btn-primary btn-sm text-center">Deliver</button></td>
                                                 </tr>
                                                 <tr>
-                                                    <td class="text-center">
-                                                        <div class="toggle-btn">
-                                                            <div class="inner-circle"></div>
-                                                        </div>
-                                                    </td>
                                                     <td>#9999999</td>
                                                     <td>Name</td>
                                                     <td>Product</td>
                                                     <td>item</td>
-                                                    <td><span class="badge bg-danger">Partially shipped</span></td>
+                                                    <td><span class="badge bg-danger">Cancelled</span></td>
                                                     <td>$6.00</td>
                                                     <td>July 12,2022</td>
+                                                    <td><a href="index-detail.php"><button type="button" class="btn btn-dark btn-sm text-center">View order</button></a></td>
+
                                                 </tr>
                                                 <tr>
-                                                    <td class="text-center">
-                                                        <div class="toggle-btn">
-                                                            <div class="inner-circle"></div>
-                                                        </div>
-                                                    </td>
                                                     <td>#9999999</td>
                                                     <td>Name</td>
                                                     <td>Product</td>
                                                     <td>item</td>
-                                                    <td><span class="badge bg-success">Fullfilled</span></td>
+                                                    <td><span class="badge bg-success">Delivered</span></td>
                                                     <td>$65.00</td>
                                                     <td>August 21,2022</td>
+                                                    <td><a href="index-detail.php"><button type="button" class="btn btn-dark btn-sm text-center">View order</button></a></td>
                                                 </tr>
                                             </tbody>
                                         </table>
+
+
                                     </div>
                                 </div>
                             </div>
