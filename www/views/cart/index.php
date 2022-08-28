1509be0fac899e976957a1e619a7e9ff566a5d3a
Leon Do <91256597+Random-Palico@users.noreply.github.com>
2022-08-28 13:32:34 +0700

    Added cart page

---
+++ www/views/cart/index.php
@@ -0,0 +1,44 @@
+<script src="https://kit.fontawesome.com/f945b90c71.js" crossorigin="anonymous"></script>
+
+<body>
+    <div class="container mt-4 p-3 rounded card d-flex">
+        <div class="row no-gutters">
+            <div class="col-12">
+                <div class="product-details me-2 w-100">
+                    <div class="d-flex flex-row align-items-center"><a href="#"><i class="fa fa-long-arrow-left"></i><span class="ms-2">Continue Shopping</span></a></div>
+                    <hr>
+                    <h6 class="mb-1 fw-bold">Shopping cart</h6>
+                    <div class="d-flex justify-content-between"><span>You have 4 items in your cart</span>
+                        <div class="d-flex flex-row align-items-center"><span class="text-black-50">Sort by:</span>
+                            <div class="price ms-2"><span class="me-1">price</span><i class="fa fa-angle-down"></i></div>
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-between align-items-center mt-3 p-2 items rounded">
+                        <div class="d-flex flex-row"><img class="rounded" src="https://i.imgur.com/QRwjbm5.jpg" width="40">
+                            <div class="ms-2"><span class="fw-bold d-block ">Iphone 11 pro</span><span class="spec">256GB, Navy Blue</span></div>
+                        </div>
+                        <div class="d-flex flex-row align-items-center"><span class="d-block">2</span><span class="d-block ms-5 fw-bold">$900</span><a href="#"><i class="fa fa-trash-o ms-3 text-black-50"></i></a></div>
+                    </div>
+                    <div class="d-flex justify-content-between align-items-center mt-3 p-2 items rounded">
+                        <div class="d-flex flex-row"><img class="rounded" src="https://i.imgur.com/GQnIUfs.jpg" width="40">
+                            <div class="ms-2"><span class="fw-bold d-block">One pro 7T</span><span class="spec">256GB, Navy Blue</span></div>
+                        </div>
+                        <div class="d-flex flex-row align-items-center"><span class="d-block">2</span><span class="d-block ms-5 fw-bold">$900</span><a href="#"><i class="fa fa-trash-o ms-3 text-black-50"></i></a></div>
+                    </div>
+                    <div class="d-flex justify-content-between align-items-center mt-3 p-2 items rounded">
+                        <div class="d-flex flex-row"><img class="rounded" src="https://i.imgur.com/o2fKskJ.jpg" width="40">
+                            <div class="ms-2"><span class="fw-bold d-block">Google pixel 4 XL</span><span class="spec">256GB, Axe black</span></div>
+                        </div>
+                        <div class="d-flex flex-row align-items-center"><span class="d-block">1</span><span class="d-block ms-5 fw-bold">$800</span><a href="#"><i class="fa fa-trash-o ms-3 text-black-50"></i></a></div>
+                    </div>
+                    <div class="d-flex justify-content-between align-items-center mt-3 p-2 items rounded">
+                        <div class="d-flex flex-row"><img class="rounded" src="https://i.imgur.com/Tja5H1c.jpg" width="40">
+                            <div class="ms-2"><span class="fw-bold d-block">Samsung galaxy Note 10&nbsp;</span><span class="spec">256GB, Navy Blue</span></div>
+                        </div>
+                        <div class="d-flex flex-row align-items-center"><span class="d-block">1</span><span class="d-block ms-5 fw-bold">$999</span><a href="#"><i class="fa fa-trash-o ms-3 text-black-50"></i></a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
